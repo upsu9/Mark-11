c9bab79db957e473202223a44548ee9331960799
upsu9 <upsu10@gmail.com>
2021-09-18 22:22:24 +0530

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('hi')
+
+var birth = document.querySelector("#btn")
+var lucky = document.querySelector(".luckyInput")
+var birthday = document.querySelector(".myBirthday")
+
+birth.addEventListener("click", clickHandler)
+function clickHandler ()
+{
+    console.log("okay you can do it")
+}
